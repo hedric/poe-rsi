a9bc96c79391c2c873df601a217dc9a7e32de1f7
Richard Hedlund <richard.hedlund@gmail.com>
2025-07-07 22:38:30 +0200

    Only allow left click using mouse wheel if ctrl is pressed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use rdev::{grab, simulate, Button, Event, EventType, SimulateError};
+use rdev::{grab, simulate, Button, Event, EventType, Key, SimulateError};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::{thread, time};
 
 static EVENT_DELAY_MS: u64 = 2;
+static CTRL_PRESSED: AtomicBool = AtomicBool::new(false);
 
 fn send(event_type: &EventType) {
     let delay = time::Duration::from_millis(EVENT_DELAY_MS);
@@ -16,14 +18,32 @@ fn send(event_type: &EventType) {
 
 fn main() {
     let callback = |event: Event| -> Option<Event> {
+        let ctrl_pressed = CTRL_PRESSED.load(Ordering::Relaxed);
+
         if let EventType::Wheel {
             delta_x: 0,
             delta_y: _,
         } = event.event_type
         {
-            send(&EventType::ButtonPress(Button::Left));
-            //println!("Detected wheel, so lets click {:?}", event);
-            send(&EventType::ButtonRelease(Button::Left));
+            if ctrl_pressed {
+                //println!("ctrl pressed and wheel, so lets click {:?}", event);
+                send(&EventType::KeyPress(Key::ControlLeft));
+                send(&EventType::ButtonPress(Button::Left));
+                send(&EventType::ButtonRelease(Button::Left));
+                send(&EventType::KeyRelease(Key::ControlLeft));
+                None
+            } else {
+                Some(event)
+            }
+        } else if let EventType::KeyPress(Key::ControlLeft) = event.event_type {
+            //println!("Detected left ctrl press {:?}", event);
+            send(&EventType::KeyPress(Key::ControlLeft));
+            CTRL_PRESSED.store(true, Ordering::Relaxed);
+            None
+        } else if let EventType::KeyRelease(Key::ControlLeft) = event.event_type {
+            //println!("Detected left ctrl release {:?}", event);
+            send(&EventType::KeyRelease(Key::ControlLeft));
+            CTRL_PRESSED.store(false, Ordering::Relaxed);
             None
         } else {
             Some(event)
